0cc37961e9cb3fb16f5e67f841a7e54e65dc4f27
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2026-08-14 17:13:45 +0100

    Desacoplar upload do SMTP indisponível

--- app/Http/Controllers/FormsController.php
+++ app/Http/Controllers/FormsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Jobs\SendFormSubmissionEmail;
 use App\Models\Form;
 use App\Models\FormData;
 use App\Http\Controllers\Traits\Iftech;
@@ -79,8 +78,6 @@ class FormsController extends Controller
         $form_data->save();
 
         $data = json_encode($form_data);
-
-        SendFormSubmissionEmail::dispatch($form_data->id)->afterResponse();
 
         //SEND BY API
 
